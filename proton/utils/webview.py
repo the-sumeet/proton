e3c6b121671815f43bdbb62f7ad3b8df4a8282fd
sumeet <Sumeet.Mathpati@steel-eye.com>
2024-12-10 13:59:41 +0530

    refacotr importing js_api app

--- proton/utils/webview.py
+++ proton/utils/webview.py
@@ -1,13 +1,12 @@
 import importlib
-
+import importlib.util
 import webview
-
+from importlib.machinery import SourceFileLoader
 from proton.runner.run import get_evaluate_js
 
 
-def get_window(app_name: str, vite_server_url: str, js_code: str):
-    module = importlib.import_module(f'{app_name}.app')
+def get_window(app_path: str, app_name: str, vite_server_url: str):
+    module = SourceFileLoader(f"{app_name}.app", f"{app_path}/app.py").load_module()
     app = getattr(module, "app")
-    window = webview.create_window("App", vite_server_url, js_api=app, )
-    print(js_code)
-    webview.start(get_evaluate_js(js_code=js_code), window, debug=True)
+    window = webview.create_window("App", vite_server_url, js_api=app)
+    webview.start(debug=True)
